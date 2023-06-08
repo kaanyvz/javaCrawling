bee085599ad2754760174dccd951f327d715bb50
kaanyvz <kaan_yavuz12@hotmail.com>
2023-06-09 00:34:32 +0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebCrawlingApplication webCrawlingApplication = new WebCrawlingApplication();
+        webCrawlingApplication.getPageLinks("https://www.skysports.com/", 0);
+        webCrawlingApplication.getArticles();
+        webCrawlingApplication.writeDataToTheFile("dataFile.txt");
     }
 }
